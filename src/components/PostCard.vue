11d946da35d78aa16505165d0550d50a4016038f
kadonomaro <denis.afer@gmail.com>
2021-06-29 23:06:37 +0700

    feat: add post-list and post-card components

---
+++ src/components/PostCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "PostCard",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <router-link :to="{ name: 'PostDetail', params: { slug: post.slug } }">
+    <article class="post-card">
+      <div class="post-card__image">
+        <img :src="post.coverImage.url" :alt="post.title" />
+      </div>
+      <div class="post-card__text">
+        <h2 class="post-card__title">{{ post.title }}</h2>
+      </div>
+    </article>
+  </router-link>
+</template>
+
+<style lang="scss">
+.post-card {
+  height: 100%;
+  border: 1px solid #f2f2f2;
+}
+
+.post-card__image {
+  img {
+    width: 100%;
+  }
+}
+
+.post-card__text {
+  padding: 10px;
+}
+
+.post-card__title {
+  margin: 0 0 10px;
+  font-size: 18px;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,51 @@
+<script>
+import gql from "graphql-tag";
+import PostCard from "@/components/PostCard";
+
+export default {
+  name: "PostList",
+  components: { PostCard },
+  apollo: {
+    posts: gql`
+      query {
+        posts {
+          id
+          slug
+          title
+          content {
+            html
+          }
+          coverImage {
+            url
+          }
+        }
+      }
+    `,
+  },
+};
+</script>
+
+<template>
+  <div class="post-list">
+    <div class="post-list__card" v-for="post in posts" :key="post.id">
+      <post-card :post="post"></post-card>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.post-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -8px;
+}
+
+.post-list__card {
+  @media screen and (min-width: 768px) {
+    flex-basis: 25%;
+    max-width: 25%;
+    padding: 0 8px 16px;
+    box-sizing: border-box;
+  }
+}
+</style>
