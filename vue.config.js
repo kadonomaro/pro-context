3480408d90cc67aa2185c160a8db22250b7f11f1
kadonomaro <denis.afer@gmail.com>
2022-03-26 11:34:01 +0700

    feat: update dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                prependData: `
-			@import "@/assets/css/variables.scss"; @import "@/assets/css/media.scss";
-		`,
+                additionalData: `@import "@/assets/css/variables.scss"; @import "@/assets/css/media.scss";`,
             },
         },
     },
